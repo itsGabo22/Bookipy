4312b795666b06f1476afb87f7dc6809873f8245
itsGabo22 <gabopaz2006@gmail.com>
2026-04-10 15:22:06 -0500

    feat: implement domain models and DTOs without Lombok

--- backend/src/main/java/com/bookipy/application/dto/ReservationDTO.java
+++ backend/src/main/java/com/bookipy/application/dto/ReservationDTO.java
@@ -1,18 +1,32 @@
 package com.bookipy.application.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ReservationDTO {
     private String guestName;
     private String guestEmail;
     private Integer roomNumber;
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
+
+    public ReservationDTO() {}
+
+    public ReservationDTO(String guestName, String guestEmail, Integer roomNumber, LocalDate checkInDate, LocalDate checkOutDate) {
+        this.guestName = guestName;
+        this.guestEmail = guestEmail;
+        this.roomNumber = roomNumber;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getGuestName() { return guestName; }
+    public void setGuestName(String guestName) { this.guestName = guestName; }
+    public String getGuestEmail() { return guestEmail; }
+    public void setGuestEmail(String guestEmail) { this.guestEmail = guestEmail; }
+    public Integer getRoomNumber() { return roomNumber; }
+    public void setRoomNumber(Integer roomNumber) { this.roomNumber = roomNumber; }
+    public LocalDate getCheckInDate() { return checkInDate; }
+    public void setCheckInDate(LocalDate checkInDate) { this.checkInDate = checkInDate; }
+    public LocalDate getCheckOutDate() { return checkOutDate; }
+    public void setCheckOutDate(LocalDate checkOutDate) { this.checkOutDate = checkOutDate; }
 }
--- backend/src/main/java/com/bookipy/domain/model/AdditionalServiceItem.java
+++ backend/src/main/java/com/bookipy/domain/model/AdditionalServiceItem.java
@@ -1,15 +1,20 @@
 package com.bookipy.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AdditionalServiceItem {
     private String type;
     private BigDecimal price;
+
+    public AdditionalServiceItem() {}
+
+    public AdditionalServiceItem(String type, BigDecimal price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 }
